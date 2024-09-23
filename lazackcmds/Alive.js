065ffcb6d07ee3594e9b4d4924d1bbc397fd4311
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-23 11:53:08 +0530

    Update Alive.js

--- lazackcmds/Alive.js
+++ lazackcmds/Alive.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender)
   var vn = 'https://cdn.jsdelivr.net/gh/Lazack28/Lazack-Device@main/jusorts/lazack.mp3'
-  let url = 'https://github.com/Lazack28/Lazack-md'
-  let murl = 'https://youtu.be/3j_EIP--2t8?si=4TFWV0On6Bl1wr-e'
-  let img = 'https://i.imgur.com/QMyKIPq.jpeg'
+  let url = 'https://wa.me/qr/P6WADRJOKAFUK1'
+  let murl = 'https://wa.me/qr/P6WADRJOKAFUK1'
+  let img = 'https://telegra.ph/file/7721cf222f9790f3fcd1b.jpg'
   let con = {
     key: {
       fromMe: false,
@@ -33,7 +33,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         title: 'ℒ𝒜𝒵𝒜𝒞𝒦 ℳ𝒟 𝒜ℒℐ𝒱ℰ',
         body: 'Lazack md',
         thumbnailUrl: img,
-        sourceUrl: 'https://chat.whatsapp.com/IIpL6gf6dcq4ial8gaJLE9',
+        sourceUrl: 'https://wa.me/qr/P6WADRJOKAFUK1',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
